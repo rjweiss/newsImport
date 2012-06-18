3a5bb2926df8a92c27d5132f4aaa6931e3faecdd
Sean Westwood <seanjw@stanford.edu>
2012-06-18 15:57:37 -0700

    changes

--- importNYTimes/src/importNYTimes.java
+++ importNYTimes/src/importNYTimes.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import javax.xml.xpath.XPathConstants;
+import org.w3c.dom.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: seanwestwood
@@ -6,4 +10,46 @@
  * To change this template use File | Settings | File Templates.
  */
 public class importNYTimes {
+    public static void main(String... args) {
+        File[] files = new File("C:/").listFiles();
+        findFiles(files);
+    }
+
+    public static void findFiles(File[] files) {
+        for (File file : files) {
+            if (file.isDirectory()) {
+                //System.out.println("Directory: " + file.getName());
+                findFiles(file.listFiles());
+            } else {
+                parse(file.getName());
+            }
+        }
+    }
+    public static void parse(String fileName){
+
+        XPathReader reader = new XPathReader(fileName);
+        String expression = "//head/meta[@name=\"print_page_number\"]";
+        String articlePageNumber = reader.read(expression,XPathConstants.STRING).toString();
+
+        expression = "//head/meta[@name=\"publication_month\"]";
+        String month = reader.read(expression,XPathConstants.STRING).toString();
+        expression = "//head/meta[@name=\"publication_day_of_month\"]";
+        String day = reader.read(expression,XPathConstants.STRING).toString();
+        expression = "//head/meta[@name=\"publication_year\"]";
+        String year = reader.read(expression,XPathConstants.STRING).toString();
+
+        String articlePublicationdate = month + "/"+ day + "/"+year;
+
+        expression = "//body/body.head/hedline/hl1";
+        String articleHeadline = reader.read(expression,XPathConstants.STRING).toString();
+
+        expression = "//body/body.content/block[@class=\"full_text\"]/p/text()";
+        String articleText = reader.read(expression,XPathConstants.STRING).toString();
+        System.out.println(articleText);
+    }
+
+
+
+
+
 }
