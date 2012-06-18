314e7dc1a5f7412453a93a1f0d6fedb29e815358
Sean Westwood <seanjw@stanford.edu>
2012-06-18 15:02:57 -0700

    put java files in the right place

---
+++ importNYTimes/src/importNYTimes.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: seanwestwood
+ * Date: 6/18/12
+ * Time: 3:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class importNYTimes {
+}
