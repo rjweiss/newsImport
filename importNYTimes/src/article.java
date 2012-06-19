cbd37421952a8fa68134848231b1b2df99c21635
Rebecca Weiss <rjweiss@stanford.edu>
2012-06-18 17:30:10 -0700

    skeleton Article class

---
+++ importNYTimes/src/article.java
@@ -0,0 +1,51 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Rebecca
+ * Date: 6/18/12
+ * Time: 5:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Article {
+
+    public String pagenum;
+    public String headline;
+    public String text;
+    public String file;
+    public String path;
+    public String type;
+    public String source;
+
+
+    public Article(String){
+
+    }
+    //article page number
+
+    public void setPageNum(String s){}
+
+    public void getPageNum(String s){}
+
+    //article headline
+    public void setHeadline(String s){}
+    public void getHeadline(String s){}
+
+    //article text
+    public void setText(String s){}
+    public void getText(String s){}
+
+    //article publication date
+    public void setPubDate(String s){}
+    public void getPubDate(String s){}
+
+    //article file
+    public void setFile(String s){}
+    public void getFile(String s){}
+
+    //article path
+    public void setPath(String s){}
+    public void getPath(String s){}
+
+
+
+
+}
