05a58ac75ee41f834f5b0027c0665aac67b0999b
Sean Westwood <seanjw@stanford.edu>
2012-06-22 16:01:45 -0700

    tribune support

--- importNYTimes/src/edu/stanford/pcl/newspaper/Importer.java
+++ importNYTimes/src/edu/stanford/pcl/newspaper/Importer.java
@@ -45,88 +45,88 @@ public class Importer {
 
     public int[] importAll(File path, String source) {
 
-	int imported = 0;
-	int skipped = 0;
+        int imported = 0;
+        int skipped = 0;
         for (File file : path.listFiles()) {
             if (file.isDirectory()) {
                 // Recursively import sub-directories. and stuff
                 int[] result = importAll(file, source);
                 //System.out.println(file.getAbsolutePath() + " (" + result[0] + ", " + result[1] + ")");
-            }
-            else {
+            } else {
                 // TODO:  Only import XML files, and probably do some sanity checking.
                 //System.out.println("Parsing " + file.getAbsolutePath() + "...");
+                int dotPos = file.getName().lastIndexOf(".");
+                String extension = file.getName().substring(dotPos);
 
-                Article article;
+                if (extension == "xml") {
+                    Article article;
 
-                try {
-                    // TODO:  Instantiate the correct Parser subclass based on some hint.
-                    if (source =="New York Times"){
-                        article = new NytParser().parse(file, source);
+                    try {
+                        // TODO:  Instantiate the correct Parser subclass based on some hint.
+                        if (source == "New York Times") {
+                            article = new NytParser().parse(file, source);
+                        } else {
+                            System.out.println("here.");
+                            article = new TribParser().parse(file, source);
+                        }
+                    } catch (Exception e) {
+                        // Parse failed, complain and skip.
+                        e.printStackTrace(System.err);
+                        skipped++;
+                        continue;
                     }
-                    else{
-                        System.out.println("here.");
-			            article = new TribParser().parse(file, source);
+
+                    if (article == null || !article.isValid()) {
+                        // Parse failed, skip.
+                        System.err.println("Parse failed or article invalid: " + file.getAbsolutePath());
+                        skipped++;
+                        continue;
                     }
-                }
-                catch (Exception e) {
-                    // Parse failed, complain and skip.
-                    e.printStackTrace(System.err);
-		    skipped++;
-		    continue;
+
+                    // Redundancy party party.
+                    try {
+                        BasicDBObject mongoObject = new BasicDBObject();
+                        mongoObject.put("pageNumber", article.getPageNumber());
+                        mongoObject.put("publicationDate", article.getPublicationDate());
+                        mongoObject.put("headline", article.getHeadline());
+                        mongoObject.put("text", article.getText());
+                        mongoObject.put("fileName", article.getFileName());
+                        mongoObject.put("mediaType", article.getMediaType());
+                        mongoObject.put("mediaSource", article.getMediaSource());
+                        mongoObject.put("overLap", article.getOverLap());
+                        mongoObject.put("status", article.getStatus());
+                        collection.insert(mongoObject, WriteConcern.SAFE);
+                        //System.out.println("Mongo insertion...");
+                    } catch (Exception e) {
+                        e.printStackTrace(System.err);
+                        continue;
+                    }
+
+                    try {
+                        Document doc = new Document();
+                        doc.add(new Field("pageNumber", article.getPageNumber(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+                        doc.add(new Field("publicationDate", article.getPublicationDate(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+                        doc.add(new Field("headline", article.getHeadline(), Field.Store.YES, Field.Index.ANALYZED));
+                        doc.add(new Field("text", article.getText(), Field.Store.YES, Field.Index.ANALYZED));
+                        doc.add(new Field("fileName", article.getFileName(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+                        doc.add(new Field("mediaType", article.getMediaType(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+                        doc.add(new Field("mediaSource", article.getMediaSource(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+                        doc.add(new Field("overLap", article.getMediaType(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+                        doc.add(new Field("status", article.getMediaType(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+                        indexWriter.addDocument(doc);
+                        //System.out.println("Lucene insertion...");
+                    } catch (IOException e) {
+                        // TODO:  Roll back insert failure (even if unlikely).
+                        e.printStackTrace(System.err);
+                    }
+                    imported++;
                 }
 
-                if (article == null || !article.isValid()) {
-                    // Parse failed, skip.
-                    System.err.println("Parse failed or article invalid: " + file.getAbsolutePath());
-		    skipped++;
-		    continue;
-                }
 
-                // Redundancy party party.
-                try {
-                    BasicDBObject mongoObject = new BasicDBObject();
-                    mongoObject.put("pageNumber", article.getPageNumber());
-                    mongoObject.put("publicationDate", article.getPublicationDate());
-                    mongoObject.put("headline", article.getHeadline());
-                    mongoObject.put("text", article.getText());
-                    mongoObject.put("fileName", article.getFileName());
-                    mongoObject.put("mediaType", article.getMediaType());
-                    mongoObject.put("mediaSource", article.getMediaSource());
-                    mongoObject.put("overLap", article.getOverLap());
-                    mongoObject.put("status", article.getStatus());
-                    collection.insert(mongoObject, WriteConcern.SAFE);
-               	    //System.out.println("Mongo insertion...");
-		}
-                catch (Exception e) {
-                    e.printStackTrace(System.err);
-		    continue;
-                }
-
-                try {
-                    Document doc = new Document();
-                    doc.add(new Field("pageNumber", article.getPageNumber(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-                    doc.add(new Field("publicationDate", article.getPublicationDate(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-                    doc.add(new Field("headline", article.getHeadline(), Field.Store.YES, Field.Index.ANALYZED));
-                    doc.add(new Field("text", article.getText(), Field.Store.YES, Field.Index.ANALYZED));
-                    doc.add(new Field("fileName", article.getFileName(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-                    doc.add(new Field("mediaType", article.getMediaType(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-                    doc.add(new Field("mediaSource", article.getMediaSource(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-                    doc.add(new Field("overLap", article.getMediaType(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-                    doc.add(new Field("status", article.getMediaType(), Field.Store.YES, Field.Index.NOT_ANALYZED));
-                    indexWriter.addDocument(doc);
-                    //System.out.println("Lucene insertion...");
-                }
-                catch (IOException e) {
-                    // TODO:  Roll back insert failure (even if unlikely).
-                    e.printStackTrace(System.err);
-                }
             }
-
-            imported++;
         }
 
-        return new int[] {imported, skipped};
+        return new int[]{imported, skipped};
     }
 
 
@@ -137,14 +137,14 @@ public class Importer {
         address.add(new ServerAddress("184.73.204.235", 27017));
         address.add(new ServerAddress("107.22.253.110", 27017));
         Mongo mongo = new Mongo(address);
-	
-	    //System.out.println("Mongo addresses:" + mongo.getAllAddress());
-	    //System.out.println("Mongo connectors:" + mongo.getConnector());
-	    //System.out.println("Mongo version:" + mongo.getVersion());
+
+        //System.out.println("Mongo addresses:" + mongo.getAllAddress());
+        //System.out.println("Mongo connectors:" + mongo.getConnector());
+        //System.out.println("Mongo version:" + mongo.getVersion());
 
         DB db = mongo.getDB(MONGO_DB_NAME);
-	
-	    //System.out.println("DB collection names :" + db.getCollectionNames());
+
+        //System.out.println("DB collection names :" + db.getCollectionNames());
 
         // Create/Open Lucene index.
         StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_36);
