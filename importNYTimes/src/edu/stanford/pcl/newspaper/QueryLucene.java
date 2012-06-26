46f0d1af84bd116d8f30046a6e3c1ef3e53e1fc4
Sean Westwood <seanjw@stanford.edu>
2012-06-26 00:21:33 -0700

    added rs support

--- importNYTimes/src/edu/stanford/pcl/newspaper/QueryLucene.java
+++ importNYTimes/src/edu/stanford/pcl/newspaper/QueryLucene.java
@@ -1,65 +1,116 @@
+package edu.stanford.pcl.newspaper;
+
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+import com.martiansoftware.jsap.*;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
- import org.apache.lucene.document.Document;
- import org.apache.lucene.document.Field;
- import org.apache.lucene.index.IndexReader;
- import org.apache.lucene.index.IndexWriter;
- import org.apache.lucene.index.IndexWriterConfig;
- import org.apache.lucene.queryParser.ParseException;
- import org.apache.lucene.queryParser.QueryParser;
- import org.apache.lucene.search.IndexSearcher;
- import org.apache.lucene.search.Query;
- import org.apache.lucene.search.ScoreDoc;
- import org.apache.lucene.search.TopScoreDocCollector;
- import org.apache.lucene.store.*;
- import org.apache.lucene.store.RAMDirectory;
- import org.apache.lucene.util.Version;
-import java.io.*;
- 
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryParser.MultiFieldQueryParser;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.SimpleFSDirectory;
+import org.apache.lucene.util.Version;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
- 
+import java.util.*;
+import java.util.Map.Entry;
+
 public class QueryLucene {
-   private static final String LUCENE_INDEX_DIRECTORY = "/rawdata/luceneindex";
-   public static void main(String[] args) throws IOException, ParseException {
+    private static final String LUCENE_INDEX_DIRECTORY = "/rawdata/luceneindex";
+    private ThreadLocal<Map<String, ArrayList>> results = new ThreadLocal<Map<String, ArrayList>>() {
+        @Override
+        protected Map<String, ArrayList> initialValue() {
+            return new HashMap<String, ArrayList>();
+        }
+    };
+
+    public void saveFile(String outputFile) throws IOException {
+        Iterator<Entry<String, ArrayList>> iterator = results.get().entrySet().iterator();
+        CSVWriter writer = new CSVWriter(new FileWriter(outputFile), '\t');
+
+        while (iterator.hasNext()) {
+            Entry entry = iterator.next();
+            ArrayList<String> row = (ArrayList) entry.getValue();
+            String[] fullRow = row.toArray(new String[row.size()]);
+            writer.writeNext(fullRow);
+        }
+        writer.close();
+    }
+
+    public Integer executeCountQuery(String[] searchTerms, String[] searchFields) throws IOException, ParseException {
         StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_36);
         Directory index = new SimpleFSDirectory(new File(LUCENE_INDEX_DIRECTORY));
-        IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_36, analyzer);
-        IndexWriter indexWriter = new IndexWriter(index, config);
+        IndexReader reader = IndexReader.open(index);
+        Query query = MultiFieldQueryParser.parse(Version.LUCENE_30, searchTerms,searchFields, analyzer);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        TopScoreDocCollector collector = TopScoreDocCollector.create(0, true);
+        searcher.search(query, collector);
 
- 
- 
-    if (args.length < 2) {
-	System.out.println("QueryLucene <attribute> <search-value>");
-	System.exit(1);
-    } 
-     String attr = args[0];
-     String value = args[1];
- 
-     Query q = new QueryParser(Version.LUCENE_36, attr, analyzer).parse(value);
- 
-     int hitsPerPage = 10;
-     IndexReader reader = IndexReader.open(index);
-     IndexSearcher searcher = new IndexSearcher(reader);
-     TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
-     searcher.search(q, collector);
-     ScoreDoc[] hits = collector.topDocs().scoreDocs;
-     
-     System.out.println("");
-     System.out.println("Top hits with \"" + value + "\" in the " + attr + ":");
-     for(int i=0;i<hits.length;++i) {
-       int docId = hits[i].doc;
-       Document d = searcher.doc(docId);
-       String text = d.get(attr);
-       int trunc = 120;
-       if (text.length() > trunc) text = text.substring(0, trunc) + "...";
-       String pad = "  ";
-       if (i < 9) pad += " ";
-       System.out.println(pad + (i + 1) + ". " + text);
-     }
-     if (collector.getTotalHits() > hits.length) {
-	System.out.println("  ... and " + (collector.getTotalHits() - hits.length) + " more.");
-     }
- 
-    searcher.close();
-   }
- 
- }
+        int count = collector.getTotalHits();
+        searcher.close();
+
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException, ParseException, JSAPException {
+        SimpleJSAP jsap = new SimpleJSAP(
+                "QueryLucene",
+                "Pulls information from Lucene",
+                new Parameter[]{
+                        new FlaggedOption("queryListFile", JSAP.STRING_PARSER, "", JSAP.REQUIRED, 'q', JSAP.NO_LONGFLAG,
+                                "List of queries to run"),
+                        new FlaggedOption("source", JSAP.STRING_PARSER, "", JSAP.REQUIRED, 's', JSAP.NO_LONGFLAG,
+                                "Sources to query"),
+                        new FlaggedOption("outputFile", JSAP.STRING_PARSER, "", JSAP.REQUIRED, 'o', JSAP.NO_LONGFLAG,
+                                "Output file"),
+                        new QualifiedSwitch("count", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.NOT_REQUIRED, 'c', "count",
+                                "Requests verbose output.").setList(true).setListSeparator(',')
+                }
+        );
+
+        JSAPResult JSAPconfig = jsap.parse(args);
+        if (jsap.messagePrinted()) System.exit(1);
+        Boolean all = JSAPconfig.getString("source").equals("all");
+
+        QueryLucene ql = new QueryLucene();
+        //CSVReader CSVReader = new CSVReader(new FileReader(JSAPconfig.getString("queryListFile")), '\t');
+        CSVReader CSVReader = new CSVReader(new FileReader("/Users/seanwestwood/Desktop/queryLucene.csv"), '\t');
+        List<String[]> queries = CSVReader.readAll();
+
+        for (String[] row : queries) {
+            ArrayList<Integer> resultRow = new ArrayList<Integer>();
+            String rowName = row[0];
+            for (String column : row) {
+                String source;
+                String[] searchFields = {"text", "mediaSource", "overLap"};
+                if ( all ){
+                    source = "New York Times";
+                    resultRow.add(ql.executeCountQuery(new String[] {column,source,"1"}, searchFields));
+                    source = "Los Angeles Times";
+                    resultRow.add(ql.executeCountQuery(new String[] {column,source,"1"}, searchFields));
+                    source = "Baltimore Sun";
+                    resultRow.add(ql.executeCountQuery(new String[] {column,source,"1"}, searchFields));
+                    source = "Chicago Tribune";
+                    resultRow.add(ql.executeCountQuery(new String[] {column,source,"1"}, searchFields));
+                }
+                else{
+                    source = JSAPconfig.getString("source");
+                    resultRow.add(ql.executeCountQuery(new String[] {column,source,"1"}, searchFields));
+                }
+
+            }
+            ql.results.get().put(rowName, resultRow);
+        }
+        ql.saveFile(JSAPconfig.getString("outputFile"));
+
+
+    }
+
+}
