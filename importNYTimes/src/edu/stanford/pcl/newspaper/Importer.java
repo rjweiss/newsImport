c88b95c86161e3782217724f7dab9404153ad5b2
Sean Westwood <seanjw@stanford.edu>
2012-06-28 23:36:20 -0700

    does not work

--- importNYTimes/src/edu/stanford/pcl/newspaper/Importer.java
+++ importNYTimes/src/edu/stanford/pcl/newspaper/Importer.java
@@ -103,7 +103,7 @@ public class Importer {
 
                     try {
                         Document doc = new Document();
-                        System.out.println(Integer.parseInt(article.getPublicationDate()));
+                        //System.out.println(Integer.parseInt(article.getPublicationDate()));
                         //DateTools.dateToString(date, Resolution.SECOND)
                         doc.add(new Field("pageNumber", article.getPageNumber(), Field.Store.YES, Field.Index.NOT_ANALYZED));
                         doc.add(new NumericField("publicationDate", 8, Field.Store.YES, true).setIntValue(Integer.parseInt(article.getPublicationDate())));
