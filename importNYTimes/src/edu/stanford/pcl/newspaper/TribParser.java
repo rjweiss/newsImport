057b0990cecbbb509d8fc92eca2aefddc28d16ba
seanwestwood <seanjw@stanford.edu>
2012-07-18 14:19:04 -0700

    lots broken, but lots fixed pushing so I can edit at home

--- importNYTimes/src/edu/stanford/pcl/newspaper/TribParser.java
+++ importNYTimes/src/edu/stanford/pcl/newspaper/TribParser.java
@@ -111,7 +111,7 @@ public class TribParser extends Parser {
                     article.setOverLap("0");
                 }
             } catch (NumberFormatException e) {
-                e.printStackTrace();  .
+                e.printStackTrace();
             }
 
         }
